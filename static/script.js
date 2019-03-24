c5f3ff32d4fe5350a8dd7724c2f7b98dce9b1bd0
Thadius <thadiusp@gmail.com>
2019-03-23 21:28:38 -0400

    Add auto timeout to responses

--- static/script.js
+++ static/script.js
@@ -9,6 +9,7 @@ let p = document.createElement('p');
 const text = document.querySelector('#text');
 text.appendChild(p);
 p.classList.add('show');
+const turnOn = document.querySelector('#data');
 
 //Function to show what I'm saying to the computer
 function showText(e) {
@@ -16,7 +17,7 @@ function showText(e) {
     .map(result => result[0])
     .map(result => result.transcript)
     .join('');
-
+  
   p.textContent = transcript;
   if (e.results[0].isFinal) {
     setTimeout(() => {
@@ -32,7 +33,6 @@ function showText(e) {
 
 //function to allow me to give phrases that will allow VA to return me information
 function matchPhrase(phrase) {
-  const turnOn = document.querySelector('#data');
   //get weather phrase
   if (phrase.includes('get the weather')) {
     while (runOnce) {
@@ -40,16 +40,7 @@ function matchPhrase(phrase) {
       getWeather();
       console.log(`Ok,I am getting the weather`);
       turnOn.classList.add('turnOn');
-    }
-  } else if (phrase.includes('thank you')) { //clear the screen phrase
-    if (!runOnce) {
-      console.log('You are welcome sir.');
-      turnOn.classList.remove('turnOn');
-      turnOn.querySelector('p').remove();
-      if ('img') {
-        turnOn.querySelector('img').remove();
-      }
-      runOnce = true;
+      timeout();
     }
   } else if (phrase.includes('get to work')) { //get the travel time to work phrase
     while (runOnce) {
@@ -58,9 +49,24 @@ function matchPhrase(phrase) {
       getTravelTime();
       console.log(`One second please,I am getting your travel time.`);
       turnOn.classList.add('turnOn');
+      timeout();
     }
   }
 };
+
+function timeout() {
+  setTimeout(
+    function() {
+      if (!runOnce) {
+      console.log('You are welcome sir.');
+      turnOn.classList.remove('turnOn');
+      turnOn.querySelector('p').remove();
+      if ('img') {
+        turnOn.querySelector('img').remove();
+      }
+      runOnce = true;
+    }}, 2000)
+}
 
 recognition.addEventListener('result', showText);
 recognition.start();
